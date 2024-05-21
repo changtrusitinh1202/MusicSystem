c9dbab2867828027ca4eb762d3d82e5250f91ad1
iuh20107161 <86941258+changtrusitinh1202@users.noreply.github.com>
2024-05-21 17:50:43 +0700

    Delete src directory

--- src/main/java/com/example/playlistservice/repository/PlaylistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.playlistservice.repository;
-
-import com.example.playlistservice.entity.Playlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-}
