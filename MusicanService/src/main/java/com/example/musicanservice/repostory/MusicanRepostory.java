7a7a37cb9c4bb91fa125992c4de83c319938359b
KhaiKhua <quangkhai24022002@gmail.com>
2024-05-23 19:49:11 +0700

    update

--- MusicanService/src/main/java/com/example/musicanservice/repostory/MusicanRepostory.java
+++
@@ -1,9 +0,0 @@
-package com.example.musicanservice.repostory;
-
-import com.example.musicanservice.entity.Musican;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MusicanRepostory extends JpaRepository<Musican,Long> {
-}
