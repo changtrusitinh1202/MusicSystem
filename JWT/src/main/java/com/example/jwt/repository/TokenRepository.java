fa3ffbec23aac00d72f7558c8db9be99256005fe
ireliaking <phamhagiahuy1708@gmail.com>
2024-05-23 20:07:35 +0700

    delete jwt and retry

--- JWT/src/main/java/com/example/jwt/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jwt.repository;
-
-
-import com.example.jwt.entity.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token, Long> {
-    Token findByToken(String token);
-}
